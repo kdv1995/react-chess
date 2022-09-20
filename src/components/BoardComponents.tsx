e1c692391aa28b5a936495310b7c0a9391754a38
kdv1995 <tribeofdanel@gmail.com>
2022-09-20 22:24:57 +0300

    setting up the project 20/09/2022

---
+++ src/components/BoardComponents.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BoardComponents = () => {
+  return (
+    <div className="board">
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+    </div>
+  );
+};
+
+export default BoardComponents;
